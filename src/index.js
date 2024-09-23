8cfcd3d377e6e9f91c80dde23bf3ffa9537a508e
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-09-23 21:18:15 +0800

    Add function to create button for the nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css"
+import {homeHeader, cafeDescription, workingHours, cafeLocation} from "./home";
+import {menuHeader, beverage, side, mainDish} from "./menu";
+import {contactUs} from "./contact";
+
+const pageContent = document.querySelector("#content");
+
+function createBtns() {
+    const btns = [
+        {text: "Home", class: "btn" },
+        {text: "Menu", class: "btn"},
+        {text: "Contact", class: "btn"}
+    ]
+
+    const buttonElements = {};
+    btns.forEach(btn => {
+        const button = document.createElement("button");
+        button.textContent = btn.text;
+        button.dataset.page = btn.text;
+        button.id = btn.text.toLowerCase();
+        button.classList.add(btn.class);
+
+        buttonElements[btn.text.toLowerCase()] = button;
+    });
+
+    return buttonElements;
+}
+
+
+
+
+
