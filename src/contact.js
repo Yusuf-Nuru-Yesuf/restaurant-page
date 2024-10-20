7431bd6b75ba8ed64673d0947a762713373024ae
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-10-20 10:58:35 +0800

    Update code base with linting and prettier

--- src/contact.js
+++ src/contact.js
@@ -1,32 +1,47 @@
-export function contactUs () {
-    const contactUsContainer = document.createElement("div");
-    contactUs.className = "contact-us"
-    const contactUsHeading = document.createElement("h1");
-    contactUsHeading.textContent = "Contact Us";
-    const contactUsList = document.createElement("ul");
-    contactUsContainer.appendChild(contactUsHeading);
-    contactUsContainer.appendChild(contactUsList);
+export function contactUs() {
+  const contactUsContainer = document.createElement("div");
+  contactUs.className = "contact-us";
+  const contactUsHeading = document.createElement("h1");
+  contactUsHeading.textContent = "Contact Us";
+  const contactUsList = document.createElement("ul");
+  contactUsContainer.appendChild(contactUsHeading);
+  contactUsContainer.appendChild(contactUsList);
 
-    const contacts = [
-        {name: "John Law", phoneNumber: "555-666-7777", occupation: "Chef", email: "john@yuya.food"},
-        {name: "Gary Tom", phoneNumber: "555-666-7776", occupation: "Manger", email: "gary@yuya.food"},
-        {name: "Lotus Nat", phoneNumber: "555-666-7775", occupation: "Waiter", email: "lotus@yuya.food"}
-    ]
+  const contacts = [
+    {
+      name: "John Law",
+      phoneNumber: "555-666-7777",
+      occupation: "Chef",
+      email: "john@yuya.food",
+    },
+    {
+      name: "Gary Tom",
+      phoneNumber: "555-666-7776",
+      occupation: "Manger",
+      email: "gary@yuya.food",
+    },
+    {
+      name: "Lotus Nat",
+      phoneNumber: "555-666-7775",
+      occupation: "Waiter",
+      email: "lotus@yuya.food",
+    },
+  ];
 
-    contacts.forEach(contact => {
-        const contactPerson = document.createElement("li");
-        contactPerson.textContent = contact.name;
-        const contactPersonOccupation = document.createElement("p");
-        contactPersonOccupation.textContent = contact.occupation;
-        const contactPersonPhone = document.createElement("p");
-        contactPersonPhone.textContent = contact.phoneNumber;
-        const contactPersonEmail = document.createElement("p");
-        contactPersonEmail.textContent = contact.email;
-        contactUsList.appendChild(contactPerson);
-        contactPerson.appendChild(contactPersonOccupation);
-        contactPerson.appendChild(contactPersonPhone);
-        contactPerson.appendChild(contactPersonEmail);
-    })
+  contacts.forEach((contact) => {
+    const contactPerson = document.createElement("li");
+    contactPerson.textContent = contact.name;
+    const contactPersonOccupation = document.createElement("p");
+    contactPersonOccupation.textContent = contact.occupation;
+    const contactPersonPhone = document.createElement("p");
+    contactPersonPhone.textContent = contact.phoneNumber;
+    const contactPersonEmail = document.createElement("p");
+    contactPersonEmail.textContent = contact.email;
+    contactUsList.appendChild(contactPerson);
+    contactPerson.appendChild(contactPersonOccupation);
+    contactPerson.appendChild(contactPersonPhone);
+    contactPerson.appendChild(contactPersonEmail);
+  });
 
-    return contactUsContainer;
+  return contactUsContainer;
 }
